e15e4d86eeb63489254d498929c1c3fb0894b7a6
Jerry <wl87617@uga.edu>
2018-09-25 11:39:13 -0400

    24.Done

---
+++ SwapPairs.cpp
@@ -0,0 +1,54 @@
+/**
+ *Author: Wenhao Lin
+ *Question:
+ *Given a linked list, swap every two adjacent nodes and return its head.
+ *
+
+
+ * Definition for singly-linked list.
+ 
+ * struct ListNode {
+ 
+ *     int val;
+ 
+ *     ListNode *next;
+
+ *     ListNode(int x) : val(x), next(NULL) {}
+
+ * };
+ */
+
+
+class SwapPairs {
+
+   public:
+    
+      ListNode* swapPairs(ListNode* head) {
+        
+          if(head==NULL || head->next ==NULL){
+            
+               return head;
+        
+          }else{
+            
+               ListNode * temphead = head;
+            
+               while(temphead!=NULL && temphead->next !=NULL){
+                
+                  int num = temphead->val;
+                
+                  temphead->val = temphead->next->val;
+                
+                  temphead->next->val = num;
+                
+                  temphead=temphead->next->next;
+            
+               }
+            
+               return head;
+        
+          }
+        
+    
+}
+};
